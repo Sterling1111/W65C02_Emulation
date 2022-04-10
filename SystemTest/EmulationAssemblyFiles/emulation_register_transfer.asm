ae02bcf8d422e963736ed0572074ddd2da8fe652
Sterling1111 <sjeppson@horizon.csueastbay.edu>
2022-04-09 17:11:16 -0700

    added files pertaining to transfer register testing

---
+++ SystemTest/EmulationAssemblyFiles/emulation_register_transfer.asm
@@ -0,0 +1,84 @@
+	.org $8000
+
+main:
+	cld
+	clc
+	cli
+	clv
+
+	ldx #$ff
+	txs
+
+	lda #$00
+	tax
+	php
+	stx $2000
+
+	lda #$7f
+	tax
+	php
+	stx $2000
+
+	lda #$ff
+	tax
+	php
+	stx $2000
+
+	lda #$00
+	tay
+	php
+	sty $2000
+
+	lda #$00
+	tay
+	php
+	stx $2000
+
+	lda #$7f
+	tay
+	php
+	stx $2000
+
+	lda #$ff
+	tay
+	php
+	sty $2000
+
+	lda #$00
+	tay
+	php
+	sty $2000
+
+	ldx #$00
+	txa
+	php
+	sta $2000
+
+	ldx #$7f
+	txa
+	php
+	sta $2000
+
+	ldx #$ff
+	txa
+	php
+	sta $2000
+
+	ldy #$00
+	tya
+	php
+	sta $2000
+
+	ldy #$7f
+	tya
+	php
+	sta $2000
+
+	ldy #$ff
+	tya
+	php
+	sta $2000
+
+	.org $fffc
+	.word main
+	.word $0000	
